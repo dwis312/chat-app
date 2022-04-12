d210c13923dbcb8d673edef9f33b008d32c6b9d4
dwis312 <dwi31287@gmail.com>
2022-04-12 23:15:11 +0700

    membuat validasi form register

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -2,18 +2,31 @@
 
 require_once __DIR__ . "/../core/Controller.php";
 require_once __DIR__ . "/../core/Request.php";
+require_once __DIR__ . "/../models/RegisterModel.php";
 
 class SiteController extends Controller
 {
     public function register(Request $request)
     {
+        $model = new RegisterModel();
+
         if ($request->post()) {
-            echo ('<pre>');
-            var_dump($_POST);
-            echo ('</pre>');
-            exit();
+            $model->loadData($request->getData());
+
+            if ($model->valiadate()) {
+                return "Success";
+            }
+
+            $this->setLayout('main');
+            return $this->render('register', [
+                'model' => $model
+            ]);
         }
-        return $this->render('register');
+
+        $this->setLayout('main');
+        return $this->render('register', [
+            'model' => $model
+        ]);
     }
 
     public function login()
--- app/views/register.php
+++ app/views/register.php
@@ -3,23 +3,39 @@
 
     <form class="form-control" action="" method="POST" autocomplete="off">
         <div class="field">
-            <input class="form-input" type="text" name="username" id="username" placeholder=" ">
+            <input class="form-input <?= $model->hasError('username') ? 'is-invalid' : ''; ?>" type="text" name="username" id="username" placeholder=" " value="<?= $model->username; ?>">
             <label class="form-label" for="username">Username</label>
+            <div class="invalid-feedback <?= $model->hasError('username') ? 'show' : ''; ?>">
+                <span class="error-icon">!!!</span>
+                <small class="error-message"><?= $model->getError('username'); ?></small>
+            </div>
         </div>
 
         <div class="field">
-            <input class="form-input" type="email" name="email" id="email" placeholder=" ">
+            <input class="form-input <?= $model->hasError('email') ? 'is-invalid' : ''; ?>" type="email" name="email" id="email" placeholder=" " value="<?= $model->email; ?>">
             <label class="form-label" for="email">Email</label>
+            <div class="invalid-feedback <?= $model->hasError('email') ? 'show' : ''; ?>">
+                <span class="error-icon">!!!</span>
+                <small class="error-message"><?= $model->getError('email'); ?></small>
+            </div>
         </div>
 
         <div class="field">
-            <input class="form-input" type="password" name="password" id="password" placeholder=" ">
+            <input class="form-input <?= $model->hasError('password') ? 'is-invalid' : ''; ?>" type="password" name="password" id="password" placeholder=" ">
             <label class="form-label" for="password">Password</label>
+            <div class="invalid-feedback <?= $model->hasError('password') ? 'show' : ''; ?>">
+                <span class="error-icon">!!!</span>
+                <small class="error-message"><?= $model->getError('password'); ?></small>
+            </div>
         </div>
 
         <div class="field">
-            <input class="form-input" type="password" name="cpassword" id="cpassword" placeholder=" ">
+            <input class="form-input <?= $model->hasError('cpassword') ? 'is-invalid' : ''; ?>" type="password" name="cpassword" id="cpassword" placeholder=" ">
             <label class="form-label" for="cpassword">Confirm Password</label>
+            <div class="invalid-feedback <?= $model->hasError('cpassword') ? 'show' : ''; ?>">
+                <span class="error-icon">!!!</span>
+                <small class="error-message"><?= $model->getError('cpassword'); ?></small>
+            </div>
         </div>
 
         <button type="submit">Register</button>
