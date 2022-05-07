fc6c1962efca6d57ef3416e2023f16579467a582
dwis312 <dwi31287@gmail.com>
2022-05-07 08:19:16 +0700

    fixing bugs routing

---
+++ app/routing/web.php
@@ -0,0 +1,32 @@
+<?php
+
+Route::get('/', [SiteController::class, 'home']);
+
+Route::post('/', [SiteController::class, 'home']);
+
+Route::get('/register', [SiteController::class, 'register']);
+
+Route::post('/register', [SiteController::class, 'register']);
+
+Route::get('/login', [SiteController::class, 'login']);
+Route::get('/login/{id}', [SiteController::class, 'login']);
+Route::get('/login/{id:\d+}/{username}', [SiteController::class, 'login']);
+
+Route::post('/login', [SiteController::class, 'login']);
+
+Route::get('/forgot', [SiteController::class, 'forgot']);
+
+Route::post('/forgot', [SiteController::class, 'forgot']);
+
+Route::get('/reset_password', [SiteController::class, 'reset_password']);
+
+Route::post('/reset_password', [SiteController::class, 'reset_password']);
+
+Route::get('/logout', [SiteController::class, 'logout']);
+
+Route::get('/profile', [SiteController::class, 'profile']);
+Route::get('/profile/{id:\d+}/{username}', [SiteController::class, 'profile']);
+
+Route::get('/chat', [SiteController::class, 'chat']);
+
+Route::post('/chat', [SiteController::class, 'chat']);
--- public/index.php
+++ public/index.php
@@ -1,41 +1,11 @@
 <?php
 
 require_once __DIR__ . "/../core/App.php";
+require_once __DIR__ . "/../app/routing/web.php";
 require_once __DIR__ . "/../app/controllers/SiteController.php";
 require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . "/../app/models/Users.php";
 
-
-Route::get('/', [SiteController::class, 'home']);
-
-Route::post('/', [SiteController::class, 'home']);
-
-Route::get('/register', [SiteController::class, 'register']);
-
-Route::post('/register', [SiteController::class, 'register']);
-
-Route::get('/login', [SiteController::class, 'login']);
-Route::get('/login/{id}', [SiteController::class, 'login']);
-Route::get('/login/{id:\d+}/{username}', [SiteController::class, 'login']);
-
-Route::post('/login', [SiteController::class, 'login']);
-
-Route::get('/forgot', [SiteController::class, 'forgot']);
-
-Route::post('/forgot', [SiteController::class, 'forgot']);
-
-Route::get('/reset_password', [SiteController::class, 'reset_password']);
-
-Route::post('/reset_password', [SiteController::class, 'reset_password']);
-
-Route::get('/logout', [SiteController::class, 'logout']);
-
-Route::get('/profile', [SiteController::class, 'profile']);
-Route::get('/profile/{id:\d+}/{username}', [SiteController::class, 'profile']);
-
-Route::get('/chat', [SiteController::class, 'chat']);
-
-Route::post('/chat', [SiteController::class, 'chat']);
 
 $config = [
     'userClass' => Users::class,
